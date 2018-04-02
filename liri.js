389bb3002af88925349d6c1de31a28a35080e831
Matthew Plein <mjplein@gmail.com>
2018-04-02 16:39:18 -0500

    final commit

---
+++ liri.js
@@ -0,0 +1,121 @@
+require("dotenv").config();
+
+// import { twitter, spotify } from "./keys";
+var keys = require("./keys.js");
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
+var request = require("request");
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+// node liri.js my-tweets
+  if (process.argv[2] == 'my-tweets') {
+var params = {screen_name: 'mjplein'};
+var recentTweets = [];
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+// This will show your last 20 tweets and when they were created at in your terminal/bash window.
+  if (!error) {
+    for (i = 0; i < 12; i++)
+    recentTweets.push(tweets[i].created_at + " " + tweets[i].text);
+  }
+  console.log(recentTweets)
+});
+}
+
+
+// node liri.js spotify-this-song '<song name here>'
+
+
+
+
+// This will show the following information about the song in your terminal/bash window
+
+
+// Artist(s)
+// The song's name
+// A preview link of the song from Spotify
+// The album that the song is from
+
+
+// If no song is provided then your program will default to "The Sign" by Ace of Base.
+// You will utilize the node-spotify-api package in order to retrieve song information from the Spotify API.
+// Like the Twitter API, the Spotify API requires you sign up as a developer to generate the necessary credentials. You can follow these steps in order to generate a client id and client secret:
+// Step One: Visit https://developer.spotify.com/my-applications/#!/
+// Step Two: Either login to your existing Spotify account or create a new one (a free account is fine) and log in.
+// Step Three: Once logged in, navigate to https://developer.spotify.com/my-applications/#!/applications/create to register a new application to be used with the Spotify API. You can fill in whatever you'd like for these fields. When finished, click the "complete" button.
+// Step Four: On the next screen, scroll down to where you see your client id and client secret. Copy these values down somewhere, you'll need them to use the Spotify API and the node-spotify-api package.
+
+
+
+// node liri.js movie-this '<movie name here>'
+if (process.argv[2] == "movie-this") {
+  function movieThis() {
+// Store all of the arguments in an array
+var nodeArgs = process.argv;
+
+// Create an empty variable for holding the movie name
+var movieName = "";
+
+// Loop through all the words in the node argument
+// And do a little for-loop magic to handle the inclusion of "+"s
+for (var i = 2; i < nodeArgs.length; i++) {
+  if (i > 2 && i < nodeArgs.length) {
+    movieName = movieName + "+" + nodeArgs[i];
+  }
+  else {
+    movieName += nodeArgs[i];
+  }
+}
+
+// Then run a request to the OMDB API with the movie specified
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+// This line is just to help us debug against the actual URL.
+console.log(queryUrl);
+
+request(queryUrl, function(error, response, body) {
+
+  // If the request is successful
+  if (!error && response.statusCode === 200) {
+
+    // Parse the body of the site and recover just the imdbRating
+    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+    console.log("Release Year: " + JSON.parse(body).Year);
+  }
+});
+}}
+// This will output the following information to your terminal/bash window:
+
+//    * Title of the movie.
+//    * Year the movie came out.
+//    * IMDB Rating of the movie.
+//    * Rotten Tomatoes Rating of the movie.
+//    * Country where the movie was produced.
+//    * Language of the movie.
+//    * Plot of the movie.
+//    * Actors in the movie.
+
+
+// If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
+
+
+// If you haven't watched "Mr. Nobody," then you should: http://www.imdb.com/title/tt0485947/
+
+// It's on Netflix!
+
+
+// You'll use the request package to retrieve data from the OMDB API. Like all of the in-class activities, the OMDB API requires an API key. You may use trilogy.
+
+
+
+// node liri.js do-what-it-says
+
+
+
+
+// Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+
+
+// It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
+// Feel free to change the text in that document to test out the feature for other commands.
